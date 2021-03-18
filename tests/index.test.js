8432dee0a30373645d2bb8770f3b5144c2e04249
Roselyn Le <roselynle95@gmail.com>
2021-03-18 11:04:33 +0000

    update layout test

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,1 +1,29 @@
+const fs = require("fs");
+const path = require("path");
+const html = fs.readFileSync(path.resolve(__dirname, "../index.html"), "utf8");
+
 global.fetch = require("jest-fetch-mock");
+
+let app;
+
+describe("app", () => {
+    beforeEach(() => {
+        document.documentElement.innerHTML = html.toString();
+        app = require("../js/index.js");
+    });
+
+    afterEach(() => {
+        fetch.resetMocks();
+    });
+
+    describe("requests", () => {
+        describe("getAllPosts", () => {
+            test("it makes a get request", () => {
+                app.getData("https://bloguefp.herokuapp.com/");
+                // expect(fetch).toHaveBeenCalledWith(expect.stringMatching(/\/posts$/))
+                expect(fetch.mock.calls[0][0]).toMatch(/$/);
+            });
+        });
+    });
+});
+// need to make sure functions in index.js are exported
