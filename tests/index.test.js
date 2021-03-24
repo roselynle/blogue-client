79c6496def5f0d738d41eddc3891934796b6c081
Roselyn Le <roselynle95@gmail.com>
2021-03-24 21:24:52 +0000

    additional tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -3,15 +3,12 @@ const path = require("path");
 const html = fs.readFileSync(path.resolve(__dirname, "../index.html"), "utf8");
 
 global.fetch = require("jest-fetch-mock");
-
-let app;
+const app = require("../assets/js/index.js");
 
 describe("app", () => {
     beforeEach(() => {
         document.documentElement.innerHTML = html.toString();
-        app = require("../js/index.js");
     });
-
     afterEach(() => {
         fetch.resetMocks();
     });
@@ -19,11 +16,34 @@ describe("app", () => {
     describe("requests", () => {
         describe("getAllPosts", () => {
             test("it makes a get request", () => {
-                app.getData("https://bloguefp.herokuapp.com/");
-                // expect(fetch).toHaveBeenCalledWith(expect.stringMatching(/\/posts$/))
+                app.getAllPosts("https://bloguefp.herokuapp.com/");
                 expect(fetch.mock.calls[0][0]).toMatch(/$/);
             });
         });
     });
+
+    describe("submitPost", () => {
+        test("it makes a post request with the users submitted data", () => {
+            const fakePost = {
+                preventDefault: jest.fn(),
+                target: {
+                    subject: { value: "Test Subject" },
+                    journalInput: { value: "Test" },
+                    gif: { value: "Test" },
+                },
+            };
+
+            app.submitPost(fakePost);
+            expect(fetch.mock.calls[0][1]).toHaveProperty("method", "POST");
+            expect(fetch.mock.calls[0][1]).toHaveProperty("body");
+        });
+    });
+    describe("sendApiRequest", () => {
+        test("it makes a call to the API", () => {
+            app.sendApiRequest(
+                "https://api.giphy.com/v1/gifs/search?api_key=${apikey}&limit=10&q="
+            );
+            expect(fetch).toHaveBeenCalled();
+        });
+    });
 });
-// need to make sure functions in index.js are exported
